a1c0b3f4d15964b227308a2a1965974e0d248ee2
ElizavetaOleynik <oleynik.elizaveta1995@gmail.com>
2020-08-14 12:40:01 +0300

    faq section done верстка, адатив, аккордион

--- js/main.js
+++ js/main.js
@@ -56,4 +56,17 @@ $(document).ready(function () {
 	});
 
 
+
+	$('.accordion-item__trigger').click(function () {
+		const parent = $(this).parent();
+
+		if (parent.hasClass('accordion-item--active')) {
+			parent.removeClass('accordion-item--active')
+		} else {
+			$('.accordion-item').removeClass('accordion-item--active')
+			parent.addClass('accordion-item--active')
+		}
+	})
+
+
 });
